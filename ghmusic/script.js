5baccde5a4b657b8ef42a77b09e4e386157be0c1
maartentbm <maartenflikkema@hotmail.com>
2017-09-03 12:53:13 +0200

    enable set page triggers from outside of menu

--- ghmusic/script.js
+++ ghmusic/script.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-	$('#menu li[data-page-id]').click(function (event) {
-		var pageId = $(event.target).closest('li').data('pageId') || 'invalid';
+	$('.set-page[data-page-id]').click(function (event) {
+		var pageId = $(event.target).closest('.set-page').data('pageId') || 'invalid';
 		$('.active').removeClass('active');
 		$('[data-page-id=' + pageId + ']').addClass('active');
 	});
-	$('#menu li[data-set-lang]').click(function (event) {
-		var li = $(event.target).closest('li'),
+	$('[data-set-lang]').click(function (event) {
+		var li = $(event.target).closest('[data-set-lang]'),
 			langId = li.data('set-lang') || 'invalid';
 		$('.selected').removeClass('selected');
 		li.addClass('selected');
