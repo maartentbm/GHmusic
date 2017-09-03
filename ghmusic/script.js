a4a765baf0de7be4a26fcb3373e9fca3782f51c1
maartentbm <maartenflikkema@hotmail.com>
2017-09-03 13:32:20 +0200

    preserve user's language setting in localStorage

--- ghmusic/script.js
+++ ghmusic/script.js
@@ -1,14 +1,22 @@
-window.onload = function() {
+window.onload = function () {
+
+	var setLanguage = function (event, langId) {
+		var li = event ? $(event.target).closest('[data-set-lang]') : null,
+			langId = li ? li.data('set-lang') || 'invalid' : langId;
+		$('.selected').removeClass('selected');
+		(li || $('[data-set-lang=' + langId + ']')).addClass('selected');
+		$('body').attr('data-lang', langId);
+		localStorage.setItem('lang', langId);
+	}
+	debugger;
+	var storedLang = localStorage.getItem('lang');
+	storedLang && ['nl', 'en'].indexOf(storedLang) > -1 && setLanguage(null, storedLang);
+
 	$('.set-page[data-page-id]').click(function (event) {
 		var pageId = $(event.target).closest('.set-page').data('pageId') || 'invalid';
 		$('.active').removeClass('active');
 		$('[data-page-id=' + pageId + ']').addClass('active');
 	});
-	$('[data-set-lang]').click(function (event) {
-		var li = $(event.target).closest('[data-set-lang]'),
-			langId = li.data('set-lang') || 'invalid';
-		$('.selected').removeClass('selected');
-		li.addClass('selected');
-		$('body').attr('data-lang', langId);
-	});
+	$('[data-set-lang]').click(setLanguage);
+
 };
